94faaf86579d4fe345c6624cd380c2339835c7de
Titouaaaan <titouanguerin@gmail.com>
2025-02-20 15:57:47 +0100

    better documentation

--- src/k_means.py
+++ src/k_means.py
@@ -5,10 +5,13 @@ from collections import Counter
 class KMeans:
     def __init__(self, k, points, max_epoch=100, eps=1e-4):
         '''
-        k: amount of clusters (classes)
-        points: data points to cluster
-        max_epoch: how many training epochs
-        eps: used to check distance between centroids for convergence
+        Initialize the KMeans clustering algorithm.
+
+        Parameters:
+        - k: Number of clusters to form.
+        - points: Data points to cluster, represented as a 2D numpy array where each row is a data point.
+        - max_epoch: Maximum number of iterations to run the algorithm.
+        - eps: Convergence threshold; the algorithm stops if the change in centroids is less than this value.
         '''
         self.k = k
         self.points = points
@@ -16,6 +19,21 @@ class KMeans:
         self.eps = eps
 
     def cluster(self):
+        '''
+        Perform the KMeans clustering algorithm.
+
+        Steps:
+        1. Initialize centroids randomly from the data points.
+        2. Iteratively update cluster assignments and centroids until convergence.
+
+        Mathematical Concepts:
+        - Euclidean Distance: Used to measure the distance between data points and centroids.
+        - Centroid: The mean of all points assigned to a cluster, representing the cluster's center.
+
+        Returns:
+        - labels: Cluster assignments for each data point.
+        - centroids: Final centroids of the clusters.
+        '''
         random_indices = np.random.choice(self.points.shape[0], self.k, replace=False) # random indices to choose the initial centroids
         centroids = self.points[random_indices]
 
@@ -34,6 +52,22 @@ class KMeans:
         return labels, centroids
     
     def plot_clusters(self, labels, centroids, assigned_labels):
+        '''
+        Visualize the clusters formed by the KMeans algorithm.
+
+        Parameters:
+        - labels: Cluster assignments for each data point.
+        - centroids: Centroids of the clusters.
+        - assigned_labels: Labels assigned to each cluster based on majority class.
+
+        Steps:
+        1. Plot the data points colored by their assigned labels.
+        2. Plot the centroids with class numbers.
+
+        Visualization:
+        - Scatter Plot: Data points are plotted in 2D space with colors representing their assigned labels.
+        - Centroids: Marked with 'X' and labeled with the cluster index.
+        '''
         plt.figure(figsize=(10, 8))
         scatter = plt.scatter(self.points[:, 0], self.points[:, 1], c=assigned_labels, cmap='viridis', alpha=0.6)
 
@@ -45,13 +79,27 @@ class KMeans:
         for i in range(self.k):
             plt.text(centroids[i, 0], centroids[i, 1], str(i), fontsize=12, ha='center', va='center', color='red')
 
-        plt.scatter(centroids[:, 0], centroids[:, 1], s=300, c='red', marker='X')
+        plt.scatter(centroids[:, 0], centroids[:, 1], s=300, c='red')
         plt.title('K-means Clustering with Assigned Labels')
         plt.xlabel('Principal Component 1')
         plt.ylabel('Principal Component 2')
         plt.show()
 
     def run_clustering(self, y_test):
+        '''
+        Run the KMeans clustering algorithm and visualize the results.
+
+        Parameters:
+        - y_test: True labels of the data points, used to assign majority class labels to clusters.
+
+        Steps:
+        1. Perform KMeans clustering.
+        2. Assign labels to clusters based on the majority class in each cluster.
+        3. Plot the clusters with assigned labels.
+
+        Returns:
+        - labels: Cluster assignments for each data point.
+        '''
         labels, centroids = self.cluster()
 
         # Assign labels to clusters based on majority class in each cluster
